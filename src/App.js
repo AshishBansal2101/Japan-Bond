64893a1a18a7443ed9140b7d96c0bf600f549f02
AshishBansal2101 <bansalashish2101@gmail.com>
2022-03-05 10:28:05 +0530

    added fade effect

--- src/App.js
+++ src/App.js
@@ -8,19 +8,33 @@ import Testimonial from "./components/Testimonial.js";
 import Pricing from "./components/Pricing.js";
 import Team from "./components/Team.js";
 import ImageCar from "./ImageCar.js";
+import Fade from "react-awesome-reveal";
+import Slide from "react-awesome-reveal";
 function App() {
   return (
     <>
       <div>
-        <Navbar />
-        <Header />
-        <Feature1 />
-        <Feature2 />
-        <ImageCar />
-        <Testimonial />
-        <Pricing />
-        <Team />
-        <Footer />
+        <fade cascade>
+          <Navbar />
+          <Header />
+          <Slide triggerOnce>
+            <Feature1 />
+          </Slide>
+          <Slide triggerOnce>
+            <Feature2 />
+          </Slide>
+          <ImageCar />
+          <Slide triggerOnce>
+            <Testimonial />
+          </Slide>
+          <Slide triggerOnce>
+            <Pricing />
+          </Slide>
+          <Slide triggerOnce>
+            <Team />
+          </Slide>
+          <Footer />
+        </fade>
       </div>
     </>
   );
